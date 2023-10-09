8293b4077e6aad08cfa0ddb90c1358882b9af7e2
yvandouenel <y.douenel@coopernet.fr>
2023-10-09 16:45:22 +0200

    début de json-server.

--- js/javascript/components/Task.js
+++ js/javascript/components/Task.js
@@ -1,15 +1,16 @@
 import ManageDom from "./ManageDom.js";
 export default class Task extends ManageDom {
-  constructor(label) {
+  constructor(label, done = false) {
     super();
     this.label = label;
+    this.done = false;
     this.dom_elements = this.render();
 
     // appel de la méthode qui gère les événements 
     this.manageEvents();
   }
   render() {
-    const section = this.createMarkup("section", "", document.body, [{ class: "task d-flex gap-3" }]);
+    const section = this.createMarkup("section", "", document.body, [{ class: "task d-flex gap-3 mt-3" }]);
     const h2 = this.createMarkup("h2", this.label, section);
 
     // Création du bouton valider
@@ -20,6 +21,7 @@ export default class Task extends ManageDom {
     // Renvoie de la référence vers les deux boutons
     return {
       section,
+      h2,
       button_validate,
       button_delete
     };
@@ -31,6 +33,18 @@ export default class Task extends ManageDom {
       this.dom_elements.section.remove();
     }
     // Gestion du click sur valider
+    this.dom_elements.button_validate.onclick = (event) => {
+      console.log(`dans onclick du bouton validate`);
+      this.done = !this.done;
+      if(this.done) {
+        this.dom_elements.h2.classList.add("strike");
+        this.dom_elements.button_validate.innerText = "Invalider";
+      }
+      else {
+        this.dom_elements.h2.classList.remove("strike");
+        this.dom_elements.button_validate.innerText = "Valider";
+      }
+    }
   }
 
 }
