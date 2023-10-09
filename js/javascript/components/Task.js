a69f320c7d9f5460fab4d8951e11de083c91c34e
yvandouenel <y.douenel@coopernet.fr>
2023-10-09 12:50:02 +0200

    exo tasks à finir.

--- js/javascript/components/Task.js
+++ js/javascript/components/Task.js
@@ -4,10 +4,23 @@ export default class Task extends ManageDom {
     super();
     this.label = label;
     this.render();
+
+    // appel de la méthode qui gère les événement 
   }
   render() {
     const section = this.createMarkup("section", "", document.body,[{class: "task"}]);
     const h2 = this.createMarkup("h2", this.label, section);
+
+    // Création du bouton valider
+
+    // Création du bouton supprimer
+
+    // Renvoie de la référence vers les deux boutons
+  }
+  manageEvents() {
+    // Gestion du click sur supprimer
+
+    // Gestion du click sur valider
   }
 
 }
