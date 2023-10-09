c80c222497d3b4e047f66a0b865e5ce61ec4bfe4
yvandouenel <y.douenel@coopernet.fr>
2023-10-09 15:08:17 +0200

    Suppression ok.

--- js/javascript/components/FormTask.js
+++ js/javascript/components/FormTask.js
@@ -3,30 +3,27 @@ import Task from "./Task.js";
 export default class FormTask extends ManageDom {
   constructor() {
     super();
-    this.dom_elements = this.render();
+    this.form_element = this.render();
     this.handleEvents();
   }
   render() {
-    const form = this.createMarkup("form", "", document.body);
+    const form = this.createMarkup("form", "", document.body,[{class: "d-flex gap-3 mt-3 align-items-center"}]);
     const label = this.createMarkup("label", "Tâche : ", form, [{ "for": "task" }]);
     const input = this.createMarkup("input", "", form, [{ "type": "text" }, { "id": "task" }, { "name": "task" }]);
-    const submit_button = this.createMarkup("input", "", form, [{ "type": "submit" }, { "value": "Ajouter" }]);
-    return {
-      form,
-      input
-    };
+    const submit_button = this.createMarkup("input", "", form, [{ "type": "submit" }, { "value": "Ajouter" },{class:"btn btn-success"}]);
+    
+    return form;
   }
   handleEvents() {
     // Si le mot clé function est employé, this représente l'instance appelante de la fonction (ici form).
 
     // Si l'expression clé => est employé, this représente l'instance de la classe dans laquelle la fonction a été déclarée.
 
-    this.dom_elements.form.onsubmit =  (event) => {
+    this.form_element.onsubmit =  (event) => {
       event.preventDefault();
       // Création de tâches ici
-      console.log(`this`, this);
       //console.log(`Dans handleEvents du FormTask`, this.dom_elements.form);
-      const formData = new FormData(this.dom_elements.form);
+      const formData = new FormData(this.form_element);
       const task = new Task(formData.get("task"));
     }
   }
--- js/javascript/components/Task.js
+++ js/javascript/components/Task.js
@@ -3,23 +3,33 @@ export default class Task extends ManageDom {
   constructor(label) {
     super();
     this.label = label;
-    this.render();
+    this.dom_elements = this.render();
 
-    // appel de la méthode qui gère les événement 
+    // appel de la méthode qui gère les événements 
+    this.manageEvents();
   }
   render() {
-    const section = this.createMarkup("section", "", document.body,[{class: "task"}]);
+    const section = this.createMarkup("section", "", document.body, [{ class: "task d-flex gap-3" }]);
     const h2 = this.createMarkup("h2", this.label, section);
 
     // Création du bouton valider
-
+    const button_validate = this.createMarkup("button", "Valider", section, [{ class: "btn btn-success ms-3" }]);
     // Création du bouton supprimer
+    const button_delete = this.createMarkup("button", "Supprimer", section, [{ class: "btn btn-danger ms-3" }]);
 
     // Renvoie de la référence vers les deux boutons
+    return {
+      section,
+      button_validate,
+      button_delete
+    };
   }
   manageEvents() {
     // Gestion du click sur supprimer
-
+    this.dom_elements.button_delete.onclick = (event) => {
+      console.log(`dans onclick du bouton delete`);
+      this.dom_elements.section.remove();
+    }
     // Gestion du click sur valider
   }
 
