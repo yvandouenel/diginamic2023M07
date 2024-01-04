b7ea296f414757f5d529c0ff27adfc60dbe1c104
yvandouenel <y.douenel@coopernet.fr>
2024-01-04 11:01:08 +0100

    début de gestion des événements sur le compteur.

--- react/counter/src/components/App.js
+++ react/counter/src/components/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Counter from './Counter';
 
 function App(props) {
   return (
     <div className="App">
       <h1>{props.title}</h1>
-      <p>date : {props.date}</p>
+      <Counter />{/* Appel du composant fonction Counter */}
     </div>
   );
 }
---
+++ react/counter/src/components/Counter.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+const Counter = () => {
+ // Destructuring de tableau
+ const [counterValue, setCounterValue] = useState(10);
+  return (
+    <section>
+      <button
+      onClick={() => {setCounterValue(counterValue - 1) }}>-</button>
+      <button>{counterValue}</button>
+      <button>+</button>
+    </section>
+  );
+}
+
+export default Counter;
