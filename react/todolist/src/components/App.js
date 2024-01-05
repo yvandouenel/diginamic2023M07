fe033847b48da03907cb0f376a6e0c4278f99aa7
yvandouenel <y.douenel@coopernet.fr>
2024-01-05 12:27:06 +0100

    Utilisation de useEffect.

--- react/todolist/src/components/App.js
+++ react/todolist/src/components/App.js
@@ -1,22 +1,29 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Task from './Task';
 import 'bootstrap/dist/css/bootstrap.css';
+import TaskFetcher from './../services/TaskFetcher';
 
 function App() {
-  // création du state tasks en appelant la fonction useState
-  const [tasks, setTasks] = useState([
-    { label: "Faire des courses", id: 1, done: false },
-    { label: "Offir des fleurs à xxx", id: 2, done: true },
-    { label: "Faire la vaisselle", id: 3, done: false }
-  ]);
+  // Utilisation du hook useEffect pour exécuter du code après le chargement du composant
+  useEffect(() => {
+    console.log(`Dans useEffect`);
+    const taskFetcher = new TaskFetcher('http://localhost:3000/tasks');
+    const promise = taskFetcher.loadTasks();
+    promise.then(tasks => {
+      setTasks(tasks);
+    })
+  }, []);
+  console.log(`Dans APP`);
+  // création du state tasks en appelant la fonction useState qui s'exécute avant le chargement du composant
+  const [tasks, setTasks] = useState([]);
 
   function handleClickValidate(taskId) {
     console.log(`Dans handleClickValidate, id : `, taskId);
     // Copie de l'état et modification en utilisant map
-    const tasksCopy = tasks.map((task)=> {
-      if(task.id === taskId) task.done = !task.done;
+    const tasksCopy = tasks.map((task) => {
+      if (task.id === taskId) task.done = !task.done;
       return task;
     })
 
@@ -30,14 +37,14 @@ function App() {
   return (
     <div className="App container">
       <h1>Gestion des tâches</h1>
-      {tasks.map((task) => 
-      <Task 
-      key={task.id} 
-      task={task} 
-      onClickValidate={handleClickValidate}
-      onClickDelete={handleClickDelete}  
+      {tasks.map((task) =>
+        <Task
+          key={task.id}
+          task={task}
+          onClickValidate={handleClickValidate}
+          onClickDelete={handleClickDelete}
 
-      />)}
+        />)}
     </div>
   );
 }
--- react/todolist/src/services/TaskFetcher.js
+++ react/todolist/src/services/TaskFetcher.js
@@ -3,13 +3,14 @@ export default class TaskFetcher {
     this.url = url;
   }
   loadTasks() {
-    fetch(this.url)
+    return fetch(this.url)
       .then((response) => {
         if (response.status === 200) return response.json();
         else throw new Error("Le serveur n'a pas répondu correctement. Statut de l'erreur : " + response.status);
       })
       .then(tasks => {
         console.log(`tasks : `, tasks);
+        return tasks;
       })
       .catch(error => {
         console.error("Erreur attrapée dans loadTasks " + error)
