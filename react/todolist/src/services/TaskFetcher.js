b8082c7a8e3dfb38bb7763d8cb65e457fb9375c9
yvandouenel <y.douenel@coopernet.fr>
2024-01-05 12:00:08 +0100

    Ajout du service fetch.

---
+++ react/todolist/src/services/TaskFetcher.js
@@ -0,0 +1,18 @@
+export default class TaskFetcher {
+  constructor(url) {
+    this.url = url;
+  }
+  loadTasks() {
+    fetch(this.url)
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        else throw new Error("Le serveur n'a pas répondu correctement. Statut de l'erreur : " + response.status);
+      })
+      .then(tasks => {
+        console.log(`tasks : `, tasks);
+      })
+      .catch(error => {
+        console.error("Erreur attrapée dans loadTasks " + error)
+      })
+  }
+}
