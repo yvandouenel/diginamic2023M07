5a792b21bae1f864fea5e59a2c2599e6df182566
yvandouenel <y.douenel@coopernet.fr>
2024-01-09 10:55:24 +0100

    Todolistts en cours - manque la gestion de l'ajout et de la suppression.

--- react/todolistts/src/components/App.tsx
+++ react/todolistts/src/components/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import TaskInterface from '../interfaces/TaskInterface';
 import Task from './Task';
 import TaskFetcher from '../services/TaskFetcher';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   const [tasks, setTasks] = useState<TaskInterface[]>([])
@@ -12,10 +13,30 @@ function App() {
         setTasks(tasks);
       })
   }, [])
+
+  function handleClickValidate(taskId: number): void {
+    console.log(`handleClickValidate`, taskId);
+
+    // Modification du state et exactement de la propriété done pour la tâche concernée
+    let propertyToPatch: Partial<TaskInterface> | null = null;
+    const tasksCopy: TaskInterface[] = tasks.map((task) => {
+      if (task.id === taskId) {
+        task.done = !task.done;
+        propertyToPatch = { done: task.done }
+      }
+      return task;
+    })
+    setTasks((currentState) => tasksCopy);
+
+    // Modification de db.json via le service
+    // Narrowing : rétrécissement, on appelle patchTask que dans le cas où propertyToPatch n'est pas
+    if(propertyToPatch) TaskFetcher.patchTask(taskId, propertyToPatch);
+  }
   return (
-    <div className="App">
+    <div className="App container">
       <h1>Todo List</h1>
-      {tasks.map((task: TaskInterface) => <Task key={task.id} task={task} />)}
+      {[...tasks].sort((a: TaskInterface, b: TaskInterface) => (Number(a.done) - Number(b.done))).map((task: TaskInterface) => <Task key={task.id} task={task} onClickValidate={handleClickValidate} />)}
+
     </div>
   );
 }
--- react/todolistts/src/components/Task.tsx
+++ react/todolistts/src/components/Task.tsx
@@ -1,11 +1,22 @@
 import TaskInterface from './../interfaces/TaskInterface';
 interface TaskProps {
   task: TaskInterface;
- }  
-const Task: React.FC<TaskProps> = ({task}) => {
+  onClickValidate: (taskId: number) => void;
+}
+const Task: React.FC<TaskProps> = ({ task, onClickValidate }) => {
   return (
-    <section>
-      <h2>{task.label}</h2>
+    <section className='d-flex justify-content-between my-3'>
+      <h2 className={task.done ? "h4 text-decoration-line-through" : "h4"}>{task.label}</h2>
+      <div >
+        <button
+          onClick={() => {
+            onClickValidate(task.id)
+          }}
+          className={task.done ? "btn btn-warning me-3" : "btn btn-success me-3"}>
+          {task.done ? "Invalider" : "Valider"}
+        </button>
+        <button className="btn btn-danger">Supprimer</button>
+      </div>
     </section>
   );
 }
